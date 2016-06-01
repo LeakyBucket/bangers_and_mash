c522a4dba70ff927b21177b8a76d68a4c016f84c
Glen Holcomb <DamnBigMan@gmail.com>
2016-06-01 16:32:35 -0600

    Adds `Acceptance` module for defining criteria for test success.

---
+++ test/lib/bangers_and_mash/web_ui/acceptance_test.exs
@@ -0,0 +1,19 @@
+defmodule BangersAndMash.WebUI.AcceptanceTest do
+  use ExUnit.Case
+  alias BangersAndMash.WebUI.Acceptance
+
+  @positive %Acceptance{type: :is, target: {:id, "body"}, value: "match"}
+  @negative %Acceptance{type: :is_not, target: {:id, "body"}, value: "match"}
+
+  test "positive assertion to string" do
+    expected = "String.contains?(inner_html({:id, \"body\"}), \"match\")"
+
+    assert ^expected = Acceptance.houndify(@positive)
+  end
+
+  test "negative assertion to string" do
+    expected = "!String.contains?(inner_html({:id, \"body\"}), \"match\")"
+
+    assert ^expected = Acceptance.houndify(@negative)
+  end
+end
